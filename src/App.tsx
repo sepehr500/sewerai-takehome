c95ef1f6da147daee3a113fd0d60a09014b039e2
sepehr500 <sepehr411@gmail.com>
2020-09-09 09:52:05 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
+
+export interface PipeParameters {
+  diameter: number;
+  material: string;
+  shape: string;
+}
+
+export interface Frame {
+  code: string;
+  frame_url: string;
+  id: string;
+  pipe_parameters: PipeParameters;
+}
+
+
+
+
+axios.defaults.baseURL = "https://tyi19eoxij.execute-api.us-west-2.amazonaws.com/prod";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [frame, setFrame] = useState<Frame | null>(null);
+  useEffect(() => {
+    axios.get<Frame>("/").then(({ data }) => {
+      setFrame(data);
+      setLoading(false);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
